c018577f70d7b16fd3baa0ba0ed2bcf584b37683
pablo <>
2016-08-23 15:54:18 +0200

    Initial code to establish p2p and send/receive data

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Net.Sockets;
 using System.Net;
+using System.IO;
 
 namespace p2pcopy
 {
@@ -8,25 +9,151 @@ namespace p2pcopy
     {
         static void Main(string[] args)
         {
-            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-            socket.Bind(new IPEndPoint(IPAddress.Any, 0));
+            P2pEndPoint p2pEndPoint = GetExternalEndPoint();
 
-            // https://gist.github.com/zziuni/3741933
+            if (p2pEndPoint == null)
+                return;
 
-            StunResult result = StunClient.Query("stun.l.google.com", 19302, socket);
-            Console.WriteLine(result.NetType.ToString());
+            Console.WriteLine("Tell this ip:port to your peer: {0}", p2pEndPoint.External.ToString());
 
-            Console.WriteLine(socket.LocalEndPoint.ToString());
+            Console.Write("Enter the ip:port of your peer: ");
+            string peer = Console.ReadLine();
 
-            if (result.NetType == StunNetType.UdpBlocked)
+            string remoteIp;
+            int remotePort;
+
+            ParseRemoteAddr(peer, out remoteIp, out remotePort);
+
+            Udt.Socket connection = PeerConnect(
+                p2pEndPoint.Internal.Port, remoteIp, remotePort);
+
+            if (connection == null)
             {
-                Console.WriteLine("Your external IP can't be obtained. You are blocked :-(");
+                Console.WriteLine("Failed to establish P2P conn to {0}", peer);
                 return;
             }
 
-            Console.WriteLine("Public endpoint: {0}. Local port: {1}",
-                result.PublicEndPoint.ToString(),
-                socket.LocalEndPoint.ToString());
+            if (args[0] == "sender")
+            {
+                RunSender(connection);
+                return;
+            }
+
+            RunReceiver(connection);
+        }
+
+        static void RunSender(Udt.Socket conn)
+        {
+            using (Udt.NetworkStream st = new Udt.NetworkStream(conn))
+            using (BinaryWriter writer = new BinaryWriter(st))
+            {
+                while (true)
+                {
+                    writer.Write("This is the info I'm sending. Isn't it cool :P " + Environment.MachineName);
+                }
+            }
+        }
+
+        static void RunReceiver(Udt.Socket conn)
+        {
+            using (Udt.NetworkStream st = new Udt.NetworkStream(conn))
+            using (BinaryReader reader = new BinaryReader(st))
+            {
+                while (true)
+                {
+                    Console.WriteLine(reader.ReadString());
+                }
+            }
+        }
+
+        static void ParseRemoteAddr(string addr, out string remoteIp, out int port)
+        {
+            string[] split = addr.Split(':');
+
+            remoteIp = split[0];
+            port = int.Parse(split[1]);
+        }
+
+        class P2pEndPoint
+        {
+            internal IPEndPoint External;
+            internal IPEndPoint Internal;
+        }
+
+        static P2pEndPoint GetExternalEndPoint()
+        {
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            try
+            {
+                socket.Bind(new IPEndPoint(IPAddress.Any, 0));
+
+                // https://gist.github.com/zziuni/3741933
+
+                StunResult externalEndPoint = StunClient.Query("stun.l.google.com", 19302, socket);
+                Console.WriteLine(externalEndPoint.NetType.ToString());
+
+                Console.WriteLine(socket.LocalEndPoint.ToString());
+
+                if (externalEndPoint.NetType == StunNetType.UdpBlocked)
+                {
+                    Console.WriteLine("Your external IP can't be obtained. You are blocked :-(");
+                    return null;
+                }
+
+                Console.WriteLine("Public endpoint: {0}. Local port: {1}",
+                    externalEndPoint.PublicEndPoint.ToString(),
+                    socket.LocalEndPoint.ToString());
+
+                return new P2pEndPoint()
+                {
+                    External = externalEndPoint.PublicEndPoint,
+                    Internal = (socket.LocalEndPoint as IPEndPoint)
+                };
+            }
+            finally
+            {
+                socket.Close();
+            }
+        }
+
+        static Udt.Socket PeerConnect(int localPort, string remoteAddr, int remotePort)
+        {
+            bool bConnected = false;
+            int retry = 0;
+
+            Udt.Socket client = null;
+
+            while (!bConnected)
+            {
+                try
+                {
+                    if (client != null)
+                        client.Close();
+
+                    client = new Udt.Socket(AddressFamily.InterNetwork, SocketType.Stream);
+                    client.ReuseAddress = true;
+
+                    client.SetSocketOption(Udt.SocketOptionName.Rendezvous, true);
+
+                    client.Bind(IPAddress.Any, localPort);
+
+                    Console.WriteLine("{0} - Trying to connect to {1}:{2}. ",
+                        retry++, remoteAddr, remotePort);
+
+                    client.Connect(remoteAddr, remotePort);
+
+                    Console.WriteLine("Connected successfully to {0}:{1}",
+                        remoteAddr, remotePort);
+
+                    bConnected = true;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            return client;
         }
 
     }
