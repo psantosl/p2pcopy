1ff3bf90c1943814f21bab06a30299eac5c5f9eb
pablo <>
2016-09-02 13:43:51 +0200

    Added version info

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("p2pcopy")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("The program to copy with NAT hole punching")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("p2pcopy")]
