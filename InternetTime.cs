4c1256156c00a8a679a7a18e8328c98769ac6e79
Alexander Schlindwein <alexander.schlindwein@hotmail.de>
2020-08-26 10:29:00 +0200

    Use NTP protocol to obtain current time
    The way p2pcopy got the current time was to scrape it from HTML.
    This is quite unreliable and also did not work anymore as the site was down.
    
    Now the NTP protocol is used to get the current time.

--- InternetTime.cs
+++ InternetTime.cs
@@ -1,38 +1,69 @@
 ﻿using System;
-using System.Text.RegularExpressions;
-using System.Net.Cache;
 using System.Net;
-using System.IO;
+using System.Net.Sockets;
 
 namespace p2pcopy
 {
     static class InternetTime
     {
-        static internal DateTime Get()
+        // https://stackoverflow.com/questions/1193955/how-to-query-an-ntp-server-using-c
+        public static DateTime Get()
         {
-            // http://stackoverflow.com/questions/6435099/how-to-get-datetime-from-the-internet
+            const string ntpServer = "pool.ntp.org";
 
-            DateTime dateTime = DateTime.MinValue;
+            // NTP message size - 16 bytes of the digest (RFC 2030)
+            var ntpData = new byte[48];
 
-            System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
+            // Setting the Leap Indicator, Version Number and Mode values
+            ntpData[0] = 0x1B; // LI = 0 (no warning), VN = 3 (IPv4 only), Mode = 3 (Client Mode)
 
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://nist.time.gov/actualtime.cgi?lzbc=siqm9b");
-            request.Method = "GET";
-            request.Accept = "text/html, application/xhtml+xml, */*";
-            request.UserAgent = "p2pcopy";
-            request.ContentType = "application/x-www-form-urlencoded";
-            request.CachePolicy = new RequestCachePolicy(RequestCacheLevel.NoCacheNoStore); //No caching
-            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-            if (response.StatusCode == HttpStatusCode.OK)
+            var addresses = Dns.GetHostEntry(ntpServer).AddressList;
+
+            // The UDP port number assigned to NTP is 123
+            var ipEndPoint = new IPEndPoint(addresses[0], 123);
+            // NTP uses UDP
+
+            using (var socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp))
             {
-                StreamReader stream = new StreamReader(response.GetResponseStream());
-                string html = stream.ReadToEnd();//<timestamp time=\"1395772696469995\" delay=\"1395772696469995\"/>
-                string time = Regex.Match(html, @"(?<=\btime="")[^""]*").Value;
-                double milliseconds = Convert.ToInt64(time) / 1000.0;
-                dateTime = new DateTime(1970, 1, 1).AddMilliseconds(milliseconds).ToLocalTime();
+                socket.Connect(ipEndPoint);
+
+                //Stops code hang if NTP is blocked
+                socket.ReceiveTimeout = 3000;
+
+                socket.Send(ntpData);
+                socket.Receive(ntpData);
+                socket.Close();
             }
 
-            return dateTime;
+            // Offset to get to the "Transmit Timestamp" field (time at which the reply 
+            // departed the server for the client, in 64-bit timestamp format."
+            const byte serverReplyTime = 40;
+
+            // Get the seconds part
+            ulong intPart = BitConverter.ToUInt32(ntpData, serverReplyTime);
+
+            // Get the seconds fraction
+            ulong fractPart = BitConverter.ToUInt32(ntpData, serverReplyTime + 4);
+
+            // Convert From big-endian to little-endian
+            intPart = SwapEndianness(intPart);
+            fractPart = SwapEndianness(fractPart);
+
+            var milliseconds = (intPart * 1000) + ((fractPart * 1000) / 0x100000000L);
+
+            // **UTC** time
+            var networkDateTime = (new DateTime(1900, 1, 1, 0, 0, 0, DateTimeKind.Utc)).AddMilliseconds((long)milliseconds);
+
+            return networkDateTime.ToLocalTime();
+        }
+
+        // stackoverflow.com/a/3294698/162671
+        static uint SwapEndianness(ulong x)
+        {
+            return (uint)(((x & 0x000000ff) << 24) +
+                           ((x & 0x0000ff00) << 8) +
+                           ((x & 0x00ff0000) >> 8) +
+                           ((x & 0xff000000) >> 24));
         }
     }
 }
